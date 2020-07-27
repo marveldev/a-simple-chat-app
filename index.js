415b44ed8ee1ef496ce3672707a3da4bfcfc9123
Marvel Dev <marvellousalika48@gmail.com>
2020-07-27 08:00:25 +0100

    add second person

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
 const textarea = document.querySelector('.textarea');
 const sendButton = document.querySelector('.send-button');
 const divContainer = document.querySelector('.div-container');
+const navSection = document.querySelector('.nav-section');
+const firstPersonButton = document.querySelector('.person1');
+const secondPersonButton = document.querySelector('.person2');
 
-function addToDom() {
-  event.preventDefault();
+function displayPersons() {
   const textareaValue = textarea.value.trim();
+  event.preventDefault();
   if (textareaValue.length > 1) {
-    const chatText = `
-      <span class="text-container">${textareaValue}</span>
-    `
-    divContainer.style.display = 'block'
-    divContainer.innerHTML += chatText;
-    textarea.value = '';
+    navSection.style.display = 'block';
   } else {
     alert('please enter a valid figure.')
   }
 }
+ 
+sendButton.addEventListener('click', displayPersons);
 
-sendButton.addEventListener('click', addToDom);
+function addToDom() {
+  event.preventDefault();
+  const textareaValue = textarea.value.trim();
+  const chatText = `
+    <div>
+      <span class="text-container">
+        ${textareaValue}<br>
+        <small>${new Date().toLocaleTimeString()} &#x2713;</small>
+      </span>
+    </div>
+  `
+  divContainer.style.display = 'block';
+  divContainer.innerHTML += chatText;
+  textarea.value = '';
+  navSection.style.display = 'none';
+}
 
+firstPersonButton.addEventListener('click', addToDom);
 
+function addToDomTwo() {
+  event.preventDefault();
+  const textareaValue = textarea.value.trim();
+    const chatText = `
+      <div style="direction:rtl">
+        <span class="span-container">${textareaValue}</span><br>
+      </div>
+      <small>${new Date().toLocaleTimeString()} &#x2713;</small>
+    `
+    divContainer.style.display = 'block';
+    divContainer.innerHTML += chatText;
+    textarea.value = '';
+    navSection.style.display = 'none';
+}
+
+secondPersonButton.addEventListener('click', addToDomTwo);
