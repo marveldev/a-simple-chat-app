5d0b229a427b7940c42d3c4fcd50c15b4bcd5924
Marvel Dev <marvellousalika48@gmail.com>
2020-12-21 20:39:20 +0100

    refactor function

--- index.js
+++ index.js
@@ -5,22 +5,42 @@ const personOptionsModal = document.querySelector('.person-options-modal');
 const firstPersonButton = document.querySelector('.person1');
 const secondPersonButton = document.querySelector('.person2');
 
-const textInput = document.querySelector('.message-input-box');
-textInput.addEventListener('keydown', () => {
-  textInput.style.height = "1px";
-  textInput.style.height = (3+textInput.scrollHeight)+"px";
-})
-
 let messageCount = 0;
 
-document.querySelector('.cancel').addEventListener('click', () => {
-  personOptionsModal.style.display = 'none';
+messageInputBox.addEventListener('keydown', () => {
+  messageInputBox.style.height = "1px";
+  messageInputBox.style.height = (3+messageInputBox.scrollHeight)+"px";
 })
 
 function displayMessageCount() {
   messageCount++
   const messageCountDiv = document.querySelector('.message-count');
   messageCountDiv.innerHTML = `message-count: ${messageCount}`;
+}
+
+sendMessageButton.addEventListener('click', () => {
+  const messageInputBoxValue = messageInputBox.value.trim();
+  if (messageInputBoxValue.length > 1) {
+    personOptionsModal.style.display = 'block';
+  } else {
+    alert('please enter a valid figure.')
+  }
+});
+
+document.querySelector('.cancel').addEventListener('click', () => {
+  personOptionsModal.style.display = 'none';
+})
+
+function displayDeleteModal() {
+  const openModalButtons = document.querySelectorAll('.open-modal-button');
+  for (let index = 0; index < openModalButtons.length; index++) {
+    const openModalButton = openModalButtons[index];
+    openModalButton.addEventListener('click', () => {
+      const messageItem = openModalButton.title;
+      const deleteModal = document.querySelector(`.${messageItem}`)
+      deleteModal.style.display = 'block';
+    })
+  }
 }
 
 function removeItem() {
@@ -34,18 +54,21 @@ function removeItem() {
       messageCount--;
       const messageCountDiv = document.querySelector('.message-count');
       messageCountDiv.innerHTML = `message-count: ${messageCount}`;
+      if (messageCount <= 1) {
+        divContainer.style.display = 'none';
+        messageCountDiv.innerHTML = '';
+      }
     })
   }
 }
 
-function displayDeleteModal() {
-  const deleteButtons = document.querySelectorAll('.delete-button');
-  for (let index = 0; index < deleteButtons.length; index++) {
-    const deleteButton = deleteButtons[index];
-    deleteButton.addEventListener('click', () => {
-      const messageItem = deleteButton.title;
-      const deleteModal = document.querySelector(`.${messageItem}`)
-      deleteModal.style.display = 'block';
+function closeDeleteModal() {
+  const closeButtons = document.querySelectorAll('.close');
+  for (let index = 0; index < closeButtons.length; index++) {
+    const closeButton = closeButtons[index];
+    closeButton.addEventListener('click', () => {
+      const deleteModal = closeButton.parentElement;
+      deleteModal.style.display = 'none';
     })
   }
 }
@@ -71,18 +94,6 @@ function changeBackground() {
   }
 }
 
-function displayPersonOptionModal() {
-  const messageInputBoxValue = messageInputBox.value.trim();
-  event.preventDefault();
-  if (messageInputBoxValue.length > 1) {
-    personOptionsModal.style.display = 'block';
-  } else {
-    alert('please enter a valid figure.')
-  }
-}
-
-sendMessageButton.addEventListener('click', displayPersonOptionModal);
-
 function addPersonOneChatToDom(event) {
   event.preventDefault();
   const itemId = 'id' + Math.random().toString(36).substring(7);
@@ -91,13 +102,12 @@ function addPersonOneChatToDom(event) {
     <div id="${itemId}" class="person-one content">
       <div class="arrow-left"></div>
       <div class="text">
-        <span>${messageInputBoxValue}</span><br>
+        <span class="message-value">${messageInputBoxValue}</span><br>
         <small>${new Date().toLocaleTimeString()} &#x2713;</small>
-        <button class="delete-button" title=${itemId}><i class="fa fa-trash"></i></button>
+        <button class="open-modal-button" title=${itemId}><i class="fa fa-trash"></i></button>
       </div>
       <div class="delete-modal ${itemId}">
-        <h3>Delete chat?</h3>
-        <p>This can't be undone and it will be removed from your permanently.</p>
+        <h2>Delete chat?</h2>
         <button class="close button">Cancel</button>
         <button class="delete button" title=${itemId}>Delete</button>
       </div>
@@ -106,39 +116,46 @@ function addPersonOneChatToDom(event) {
   divContainer.style.display = 'block';
   divContainer.innerHTML += messageItem;
   messageInputBox.value = '';
+  messageInputBox.style.height = '';
   personOptionsModal.style.display = 'none';
-  displayMessageCount()
-  displayDeleteModal()
+  displayMessageCount();
+  displayDeleteModal();
   removeItem();
+  closeDeleteModal();
 } 
 
 firstPersonButton.addEventListener('click', addPersonOneChatToDom);
 
 function addPersonTwoChatToDom(event) {
   event.preventDefault();
+  const itemId = 'id' + Math.random().toString(36).substring(7);
   const messageInputBoxValue = messageInputBox.value.trim();
   const messageItem = `
-    <div class="person-two content">
+    <div id="${itemId}" class="person-two content">
       <div class="arrow-right"></div>
       <div class="text">
-        <span>${messageInputBoxValue}</span><br>
+        <span class="message-value">${messageInputBoxValue}</span><br>
         <small>${new Date().toLocaleTimeString()} &#x2713;</small>
-        <button class="delete-button"><i class="fa fa-trash"></i></button>
+        <button class="open-modal-button" title=${itemId}>
+          <i class="fa fa-trash"></i>
+        </button>
       </div>
-      <div class="delete-modal">
-        <h3>Delete chat?</h3>
-        <p>This can't be undone and it will be removed from your permanently.</p>
+      <div class="delete-modal ${itemId}">
+        <h2>Delete chat?</h2>
         <button class="close button">Cancel</button>
-        <button class="delete button">Delete</button>
+        <button class="delete button" title=${itemId}>Delete</button>
       </div>
     </div>
   `
   divContainer.style.display = 'block';
   divContainer.innerHTML += messageItem;
   messageInputBox.value = '';
+  messageInputBox.style.height = '';
   personOptionsModal.style.display = 'none';
-  displayMessageCount()
+  displayMessageCount();
+  displayDeleteModal();
   removeItem();
+  closeDeleteModal();
 }
 
 secondPersonButton.addEventListener('click', addPersonTwoChatToDom);
