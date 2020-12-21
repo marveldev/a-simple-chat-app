5753bc5eedbf49a988706320e878476558fa674d
Marvel Dev <marvellousalika48@gmail.com>
2020-12-21 15:18:35 +0100

    add display delete modal function
    -refactor addPersonOneChat function
    -refactor addPersonTwoChat function

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const messageInputBox = document.querySelector('.message-input-box');
-const sendMessageButton = document.querySelector('.send-button');
+const sendMessageButton = document.querySelector('.send');
 const divContainer = document.querySelector('.div-container');
 const personOptionsModal = document.querySelector('.person-options-modal');
 const firstPersonButton = document.querySelector('.person1');
 const secondPersonButton = document.querySelector('.person2');
-const closeModalButton= document.querySelector('.cancel');
+
+const textInput = document.querySelector('.message-input-box');
+textInput.addEventListener('keydown', () => {
+  textInput.style.height = "1px";
+  textInput.style.height = (3+textInput.scrollHeight)+"px";
+})
 
 let messageCount = 0;
 
-closeModalButton.addEventListener('click', () => {
-  event.preventDefault();
+document.querySelector('.cancel').addEventListener('click', () => {
   personOptionsModal.style.display = 'none';
 })
 
@@ -20,15 +24,28 @@ function displayMessageCount() {
 }
 
 function removeItem() {
+  const deleteButtons = document.querySelectorAll('.delete');
+  for (let index = 0; index < deleteButtons.length; index++) {
+    const deleteButton = deleteButtons[index];
+    deleteButton.addEventListener('click', () => {
+      const messageItem = deleteButton.title;
+      const itemDiv = document.querySelector(`#${messageItem}`)
+      divContainer.removeChild(itemDiv);
+      messageCount--;
+      const messageCountDiv = document.querySelector('.message-count');
+      messageCountDiv.innerHTML = `message-count: ${messageCount}`;
+    })
+  }
+}
+
+function displayDeleteModal() {
   const deleteButtons = document.querySelectorAll('.delete-button');
   for (let index = 0; index < deleteButtons.length; index++) {
     const deleteButton = deleteButtons[index];
     deleteButton.addEventListener('click', () => {
-      const messageItem = deleteButton.parentElement;
-      divContainer.removeChild(messageItem);
-      messageCount--;
-      const messageCountDiv = document.querySelector('.message-count');
-      messageCountDiv.innerHTML = `message-count: ${messageCount}`;
+      const messageItem = deleteButton.title;
+      const deleteModal = document.querySelector(`.${messageItem}`)
+      deleteModal.style.display = 'block';
     })
   }
 }
@@ -66,14 +83,24 @@ function displayPersonOptionModal() {
 
 sendMessageButton.addEventListener('click', displayPersonOptionModal);
 
-function addPersonOneChatToDom() {
+function addPersonOneChatToDom(event) {
   event.preventDefault();
+  const itemId = 'id' + Math.random().toString(36).substring(7);
   const messageInputBoxValue = messageInputBox.value.trim();
   const messageItem = `
-    <div class="text-container">
-      <span>${messageInputBoxValue}</span><br>
-      <small>${new Date().toLocaleTimeString()} &#x2713;</small>
-      <button class="delete-button"><i class="fa fa-trash"></i></button>
+    <div id="${itemId}" class="person-one content">
+      <div class="arrow-left"></div>
+      <div class="text">
+        <span>${messageInputBoxValue}</span><br>
+        <small>${new Date().toLocaleTimeString()} &#x2713;</small>
+        <button class="delete-button" title=${itemId}><i class="fa fa-trash"></i></button>
+      </div>
+      <div class="delete-modal ${itemId}">
+        <h3>Delete chat?</h3>
+        <p>This can't be undone and it will be removed from your permanently.</p>
+        <button class="close button">Cancel</button>
+        <button class="delete button" title=${itemId}>Delete</button>
+      </div>
     </div>
   `
   divContainer.style.display = 'block';
@@ -81,19 +108,29 @@ function addPersonOneChatToDom() {
   messageInputBox.value = '';
   personOptionsModal.style.display = 'none';
   displayMessageCount()
+  displayDeleteModal()
   removeItem();
 } 
 
 firstPersonButton.addEventListener('click', addPersonOneChatToDom);
 
-function addPersonTwoChatToDom() {
+function addPersonTwoChatToDom(event) {
   event.preventDefault();
   const messageInputBoxValue = messageInputBox.value.trim();
   const messageItem = `
-    <div class="text-container content">
-      <span>${messageInputBoxValue}</span><br>
-      <small>${new Date().toLocaleTimeString()} &#x2713;</small>
-      <button class="delete-button"><i class="fa fa-trash"></i></button>
+    <div class="person-two content">
+      <div class="arrow-right"></div>
+      <div class="text">
+        <span>${messageInputBoxValue}</span><br>
+        <small>${new Date().toLocaleTimeString()} &#x2713;</small>
+        <button class="delete-button"><i class="fa fa-trash"></i></button>
+      </div>
+      <div class="delete-modal">
+        <h3>Delete chat?</h3>
+        <p>This can't be undone and it will be removed from your permanently.</p>
+        <button class="close button">Cancel</button>
+        <button class="delete button">Delete</button>
+      </div>
     </div>
   `
   divContainer.style.display = 'block';
