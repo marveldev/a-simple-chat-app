2c186e03eb172165ea79a4fcb7b0355df85d5fea
Marvel Dev <marvellousalika48@gmail.com>
2020-12-24 06:46:19 +0100

    remove console message

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import { request } from './dataStorage.js';
-import { chatEventListeners, displayItemFromDb, formEventListeners } from './events.js';
+import { chatEventListeners, displayItemFromDb, formEventListeners }
+from './events.js';
 
 request.onsuccess = () => {
-  console.log('ok');
   chatEventListeners();
   formEventListeners();
   displayItemFromDb();
